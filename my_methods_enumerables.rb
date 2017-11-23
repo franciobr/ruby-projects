ec3403206056bf58cff0d21abd9da2c6084d28ae
francio_rodrigues <franciobr@gmail.com>
2017-11-23 20:54:22 -0200

    added a few my_methods to the file

--- my_methods_enumerables.rb
+++ my_methods_enumerables.rb
@@ -1,9 +1,60 @@
-def Enumerable
-  def my_each(iterable)
+module Enumerable
+  #  def my_each(iterable) NOPE. It is going to demand an iterable input which is NOT self.
+
+  def my_each
     index = 0
-    while index<iterable.length
-      yield iterable[index]
+
+    #using a for here would be cheating because the for loop calls the each method under the hood, which is forbidden by ODIN HIMSELF!!! Hope while does not do that because that would be cheating.
+
+    #cannot be <=. self[length] = nil. self[length-1] = last
+    while index<self.length
+      yield self[index]
       index += 1
     end
+
+    return self#yeah, the each method does this.
   end
+
+  def my_each_with_index
+    index = 0
+
+    while index<self.length
+      yield self[index], index
+      index += 1
+    end
+
+    return self#yeah, the each method does this.
+  end
+
+  def my_select(&block)
+    output = []
+    self.my_each do |element|
+
+      if block.call(element)==true #I am explicit.
+        output << element
+      end
+    end
+
+    return output
+  end
+  
+  def my_select2
+    output = []
+    self.my_each do |element|
+
+      if yield(element) ==true #I am explicit.
+        output << element
+      end
+    end
+
+    return output
+  end
+  
+  def my_all?(&block)
+    self.my_each do |element|
+      if block.call(element)==false
+        return false
+      end    
+  end
+
 end
