d3e4333dba99017a02014a2d372c77b2febbedb2
francio_rodrigues <franciobr@gmail.com>
2017-11-23 19:18:20 -0200

    Adicionei os arquivos hidden e os checkpoints do notebook

--- substring.rb
+++ substring.rb
@@ -1,5 +1,5 @@
 #Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
-#does git sees my comment =P?
+
 #---PSEUDO CODE---
 #generate a substring
 #look for substring in dictionary
